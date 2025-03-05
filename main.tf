dd588dfbe9333cc052f161cc207cd2f3b790bd9f
Amit Kumar <amitkumarsingh843412@gmail.com>
2025-03-05 14:39:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,6 +57,14 @@ resource "aws_security_group" "websg" {
   name   = "web"
   vpc_id = aws_vpc.myvpc.id
 
+ingress {
+description = "HTPS"
+from_port = 443
+to_port   = 443
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
   ingress {
     description = "HTTP form VPC"
     from_port   = 80
@@ -77,6 +85,8 @@ resource "aws_security_group" "websg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+
   }
 
   tags = {
